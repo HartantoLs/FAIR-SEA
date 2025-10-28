f96e9479530c07fef7646d12da585f070ab38221
HartantoLs <117240954+HartantoLs@users.noreply.github.com>
2025-10-28 16:12:58 +0800

    Initiate streamlit app.py

---
+++ app.py
@@ -0,0 +1,7 @@
+# app.py
+import streamlit as st
+
+st.set_page_config(layout="wide")
+st.title("FAIR-SEA: Bias Analysis Dashboard")
+
+st.info("Dashboard is under construction. Please add components.")
